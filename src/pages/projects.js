3eacd374d4d9b9f450b7bdba948088b9b9996c5d
Abhi Velaga <abhinav.velaga@gmail.com>
2021-04-14 02:11:14 -0500

    added photos

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,51 +1,74 @@
-import img1 from '../assets/img1.jpg';
+import p1 from '../assets/p1.jpg';
+import p2 from '../assets/p2.jpg';
+import p3 from '../assets/p3.jpg';
+import p4 from '../assets/p4.jpg';
+import p5 from '../assets/p5.jpg';
+import p6 from '../assets/p6.jpg';
+import p7 from '../assets/p7.jpg';
+import p8 from '../assets/p8.jpg';
+import p9 from '../assets/p9.jpg';
+import p10 from '../assets/p10.jpg';
+import p11 from '../assets/p11.jpg';
+import p12 from '../assets/p12.jpg';
+import p13 from '../assets/p13.jpg';
+import p14 from '../assets/p14.jpg';
+import p15 from '../assets/p15.jpg';
+import p16 from '../assets/p16.jpg';
+import p17 from '../assets/p17.jpg';
+import p18 from '../assets/p18.jpg';
+import p19 from '../assets/p19.jpg';
+import p20 from '../assets/p20.jpg';
+import p21 from '../assets/p21.jpg';
+import p22 from '../assets/p22.jpg';
+import p23 from '../assets/p23.jpg';
+import p24 from '../assets/p24.jpg';
 
 export const projects = [{
     "url": "project1",
-    "imgs": [img1, img1, img1],
-    "cover": img1,
+    "imgs": [p1, p2, p3],
+    "cover": p1,
     "text": "this is my first project and i'm so proud of it because it's so awesome and amazing."
   },
   {
     "url": "project2",
-    "imgs": [img1, img1],
-    "cover": img1,
+    "imgs": [p4, p5,p6],
+    "cover": p4,
     "text": "now this one will blow your mind even more than the past one. i spent a lot of time on it and used a new medium."
   },
   {
     "url": "project3",
-    "imgs": [img1, img1],
-    "cover": img1,
+    "imgs": [p7, p8, p9],
+    "cover": p7,
     "text": "forget what i said about project 2, i broke new ground on this one. the world has never seen anything like it before."
   },
   {
     "url": "project4",
-    "imgs": [img1, img1],
-    "cover": img1,
+    "imgs": [p10, p11, p12],
+    "cover": p10,
     "text": "this project could be the description of mind blowing. i spent day and night making it the best it can be."
   },
   {
     "url": "project5",
-    "imgs": [img1, img1],
-    "cover": img1,
+    "imgs": [p13,p14,p15],
+    "cover": p13,
     "text": "this is from a rough patch in my life after my girlfriend dumped me and i sought meaning in my surroundings. the architecture of the world relaly spoke to me."
   },
   {
     "url": "project6",
-    "imgs": [img1, img1],
-    "cover": img1,
+    "imgs": [p16, p17, p18],
+    "cover": p16,
     "text": "ha i don't have depression anymore i'm completely cured. sunshine and lots of uppers."
   },
   {
     "url": "project7",
-    "imgs": [img1, img1],
-    "cover": img1,
+    "imgs": [p19, p20, p21],
+    "cover": p19,
     "text": "hello is this thing on?"
   },
   {
     "url": "project8",
-    "imgs": [img1, img1],
-    "cover": img1,
+    "imgs": [p22, p23, p24],
+    "cover": p22,
     "text": "i hate myself."
   },
 ];
